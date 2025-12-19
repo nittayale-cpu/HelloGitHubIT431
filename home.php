9a0f58b62b584f9d56ad7c626938cd32858ff441
Nittaya <nittaya.le@rmutsvmail.com>
2025-12-19 03:51:14 +0000

    Edit file#6

--- home.php
+++ home.php
@@ -25,7 +25,7 @@
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a href="facilitate.php">สิ่งอำนวยความสะดวก</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
-				<li><a href="contact.php">ติดต่ออออออ</li>
+				<li><a href="contact.php">ติดต่อออออออออ</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
